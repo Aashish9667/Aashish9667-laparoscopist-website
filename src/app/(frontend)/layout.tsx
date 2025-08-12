5152156820ee373a1e30b86f0d493f52498c07a6
sadik-malik <malik0786sam@gmail.com>
2025-08-12 12:36:18 +0530

    feat: added next-themes and react-hook-form

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -8,7 +8,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props;
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link href="/favicon.ico" rel="icon" sizes="any" />
         <link href="/icon.svg" rel="icon" type="image/svg+xml" />
--- src/app/(frontend)/providers.tsx
+++ src/app/(frontend)/providers.tsx
@@ -5,19 +5,22 @@ import { ProgressProvider } from '@bprogress/next/app';
 import cssVariables from '@/lib/css-variables';
 import oklchToHex from '@/lib/color-manipulation/oklch-to-hext';
 import { QueryClientProvider } from '@/components/piplup-jsrepo/tanstack-query';
+import { ThemeProvider } from 'next-themes';
 
 export type ProvidersProps = { children: React.ReactNode };
 
 export default function Providers(props: ProvidersProps) {
   const { children } = props;
   return (
-    <ProgressProvider
-      color={oklchToHex(cssVariables.colors.primary)}
-      height={`${cssVariables.bprogress.height || 4}px`}
-      options={{ showSpinner: false }}
-      shallowRouting
-    >
-      <QueryClientProvider>{children}</QueryClientProvider>
-    </ProgressProvider>
+    <ThemeProvider>
+      <ProgressProvider
+        color={oklchToHex(cssVariables.colors.primary)}
+        height={`${cssVariables.bprogress.height || 4}px`}
+        options={{ showSpinner: false }}
+        shallowRouting
+      >
+        <QueryClientProvider>{children}</QueryClientProvider>
+      </ProgressProvider>
+    </ThemeProvider>
   );
 }
