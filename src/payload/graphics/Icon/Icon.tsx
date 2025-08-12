c073bd024a22d0f2dd75087397b54a9f757287a1
sadik-malik <malik0786sam@gmail.com>
2025-08-12 12:40:15 +0530

    feat: replaced lucide-react with react-icons

--- src/payload/graphics/Icon/Icon.tsx
+++ src/payload/graphics/Icon/Icon.tsx
@@ -1,16 +1,14 @@
-import * as React from 'react'
-import { Scale } from 'lucide-react'
-import classes from './Icon.module.css'
+import * as React from 'react';
+import { LuScale } from 'react-icons/lu';
+import classes from './Icon.module.css';
 
 export default function Icon() {
   return (
     <div className={classes.logoWrap}>
       <div className={classes.iconWrap}>
-        <Scale className={classes.icon} />
+        <LuScale className={classes.icon} />
       </div>
-      <span className={classes.srOnly}>
-        Vivek Narayan Sharma - Advocate, Supreme Court of India
-      </span>
+      <span className={classes.srOnly}>Logo</span>
     </div>
-  )
+  );
 }
