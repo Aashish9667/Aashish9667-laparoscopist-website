f33c592604043c4d80b7d4e817856ebd27080a08
code with harry <aashish.kumar@96672gmail.com>
2025-10-12 00:31:26 +0530

    feat: welcome folder file hero-section changes

--- src/components/frontend/welcome/hero-section.tsx
+++ src/components/frontend/welcome/hero-section.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 });
 export default function Homehero() {
   return (
-    <div className="relative w-full md:h-[677px] h-full mt-20">
+    <div className="relative w-full md:h-[677px] lg:h-[677px] h-full mt-20">
       {/* Background Image */}
       <div className="absolute inset-0">
         <Image
@@ -25,17 +25,17 @@ export default function Homehero() {
       {/* Overlay content */}
       <div className="relative z-10 flex flex-col md:flex-row items-center justify-between h-full max-w-[1200px] mx-auto px-4 md:px-8 sm:px-8 gap-10 sm:justify-start sm:items-start">
         {/* Left Text */}
-        <div className="md:text-left md:w-1/2 sm:text-left md:mt-30">
+        <div className="lg:text-lft lg:w-1/2 md:text-left md:w-4/2 sm:text-left md:mt-30">
           <div className={poppins.className}>
-            <h1 className="text-[27px] md:text-[40px] md:leading-[48px] leading-tight font-bold text-[rgb(19,34,36)] md:mb-20 md:mt-20 mt-20 pl-3 sm:pl-0">
+            <h1 className="text-[rgb(19,34,36)] text-[27px] leading-tight font-bold mt-20 pl-3 md:text-[40px] md:leading-[48px] md:mb-20 md:mt-20 lg:leading-[48px] lg:text-[40px] lg:mb-20 lg:mt-20 sm:pl-0">
               Redefining Surgical Excellence with <br className="hidden md:hidden sm:block" />{' '}
               Advanced Laparoscopic Solutions
             </h1>
           </div>
 
           <div className={`hidden sm:hidden md:block ${poppins.className}`}>
-            <div className="text-gray-800 font-semibold text-lg md:text-[23px] md:mt-0 mt-5 ">
-              <p className="bg-gray-900 text-[rgb(255,255,255)] md:p-3 md:pr-5 px-2 py-1 rounded-r-3xl  w-fit">
+            <div className="text-gray-800 font-semibold text-lg md:text-[23px] md:-mt-10 ">
+              <p className="bg-gray-900 text-[rgb(255,255,255)] md:p-3 md:pr-5 px-2 py-1 rounded-r-3xl whitespace-nowrap w-fit">
                 Dr. Medhavi and Dr. Monika Tomar
               </p>
             </div>
@@ -51,7 +51,7 @@ export default function Homehero() {
 
         {/* Right Image */}
 
-        <div className=" relative w-full h-[350px] md:w-[590px] md:h-[720px] sm:w-[590px] sm:h-[550px]  md:-mt-10  -mt-20 flex justify-end items-end sm:justify-start sm:items-start">
+        <div className=" relative w-full h-[350px] lg:w-[590px] lg:h-[720px] md:w-[990px] md:h-[720px] sm:w-[590px] sm:h-[550px]  md:-mt-10  -mt-20 flex justify-end items-end sm:justify-start sm:items-start">
           <Image
             alt="Doctors"
             className="object-contain md:object-bottom sm:object-left-top"
@@ -60,7 +60,7 @@ export default function Homehero() {
           />
         </div>
         {/* mobile bottom text with sticky image bottom  */}
-        <div className="md:hidden -mt-10 ml-3 mb-15 sm:-mt-10 sm:-ml-4 sm:mb-15">
+        <div className="md:hidden -mt-10 ml-3 mb-15 sm:-mt-10 sm:-ml-4 sm:mb-15 flex flex-col ">
           {/* Main Title */}
           <div
             className={`inline-block bg-[#0f2646] rounded-r-3xl sm:px-4 sm:py-4 sm:w-150 px-4 py-4 w-80 ${poppins.className}`}
@@ -74,7 +74,7 @@ export default function Homehero() {
 
           {/* Sub Title */}
           <div
-            className={`inline-block bg-white rounded-br-3xl sm:px-4 sm:py-[6px] px-4 py-[6px] ${poppins.className}`}
+            className={`inline-block bg-white rounded-br-3xl sm:px-4 sm:py-[6px] px-4 py-[6px] w-fit ${poppins.className}`}
           >
             <p className="text-[16px] text-[#000000de] font-normal leading-5">
               Laparoscopic Surgeons
