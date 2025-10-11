056dc22f10db3b0385323fe1d20af1339c88615f
code with harry <aashish.kumar@96672gmail.com>
2025-10-11 22:51:03 +0530

    feat: welcome folder file laparoscopy-surgeons changes

--- src/components/frontend/welcome/laparoscopy-surgeons/laparoscopy-surgeons.tsx
+++ src/components/frontend/welcome/laparoscopy-surgeons/laparoscopy-surgeons.tsx
@@ -10,19 +10,19 @@ const poppins = Poppins({
 
 export default function LaparoscopySurgeons() {
   return (
-    <div className="bg-gray-300 md:px-20 md:py-20 p-5 sm:px-8 sm:py-20 px-8 py-20">
+    <div className="bg-gray-300 lg:px-20 lg:py-20 p-5 sm:px-8 sm:py-20 px-8 py-20">
       <div className="max-w-[1200px] mx-auto">
         {/* Parent Flex Container */}
-        <div className="flex flex-col md:flex-row items-start justify-between">
+        <div className="flex flex-col lg:flex-row items-start justify-between">
           {/* Left Section */}
-          <div className={`${poppins.className} md:w-1/2 flex flex-col justify-start`}>
-            <h1 className="text-[14px] md:text-[16px] font-semibold mb-4 text-[#24426e] leading-[24px]">
+          <div className={`${poppins.className} lg:w-1/2 flex flex-col justify-start`}>
+            <h1 className="text-[14px] lg:text-[16px] font-semibold mb-4 text-[#24426e] leading-[24px]">
               Laparoscopy Surgeons
             </h1>
-            <p className="text-[32px] md:text-[32px] mb-3 text-[#000000de] font-medium leading-[38px]">
+            <p className="text-[32px] lg:text-[32px] mb-3 text-[#000000de] font-medium leading-[38px]">
               Your Expert in Minimally Invasive Surgery
             </p>
-            <p className="text-[16px] md:text-[16px] text-[#00000099] leading-[26px]">
+            <p className="text-[16px] lg:text-[16px] text-[#00000099] leading-[26px]">
               A team of dedicated laparoscopic surgeons with extensive experience, offering
               advanced, minimally invasive procedures to ensure faster recovery and improved patient
               outcomes.
@@ -30,11 +30,11 @@ export default function LaparoscopySurgeons() {
           </div>
 
           {/* Right Section */}
-          <div className="md:w-1/2 flex flex-col gap-6 justify-center md:justify-end md:items-end pt-5 md:pt-0 ">
+          <div className="lg:w-1/2 flex flex-col gap-6 justify-center lg:justify-end lg:items-end pt-5 lg:pt-0 ">
             {/* Doctor 1 */}
-            <div className="relative md:w-[490px] md:h-[170px] sm:w-[608px] sm:h-[225px] w-75 h-55 bg-white rounded-3xl shadow-md overflow-visible flex flex-col items-center md:block mt-12 md:mt-0">
+            <div className="relative xl:w-[490px] xl:h-[170px] lg:w-[390px] lg:h-[170px] md:w-max-[710px] md:h-max-[225px]  sm:w-max-[608px] sm:h-max-[225px] w-min-75 h-min-55 bg-white rounded-3xl shadow-lg overflow-visible flex flex-col items-center lg:block mt-12 lg:mt-0">
               {/* Image Overlapping Top (mobile) */}
-              <div className="absolute md:hidden -top-10 flex justify-center w-full z-10">
+              <div className="absolute lg:hidden -top-10 flex justify-center w-full z-10">
                 <Image
                   alt="Dr. Medhavi"
                   className="rounded-full border-2 border-[#24426d]"
@@ -47,26 +47,26 @@ export default function LaparoscopySurgeons() {
               {/* Desktop Overlapping (left) */}
               <Image
                 alt="Dr. Medhavi"
-                className="hidden md:block rounded-full h-30 w-30 absolute -left-12 top-1/2 transform -translate-y-1/2 border-2 border-[#24426d] z-10"
+                className="hidden lg:block rounded-full h-30 w-30 absolute -left-12 top-1/2 transform -translate-y-1/2 border-2 border-[#24426d] z-10"
                 height={96}
                 src="/Medhavi-Tomar.png.avif"
                 width={96}
               />
 
               <div
-                className={`${poppins.className} px-6 py-6 ms:px-6 md:pl-23 pl-5 h-full flex flex-col justify-center text-center md:text-left`}
+                className={`${poppins.className} px-6 py-6 lg:px-6 lg:pl-23 pl-5 h-full flex flex-col justify-center text-center lg:text-left`}
               >
-                <h1 className="font-semibold text-[20px] text-[#00000099] md:pb-2 mt-8 sm:mt-5 md:mt-0 pb-2">
+                <h1 className="font-semibold text-[20px] text-[#00000099] lg:pb-2 mt-8 sm:mt-10 lg:mt-0 pb-2">
                   Dr. Medhavi Tomar
                 </h1>
-                <p className="text-[16px] text-[#00000099] md:pb-2 font-normal">
+                <p className="text-[16px] text-[#00000099] lg:pb-2 font-normal">
                   M.B.B.S., M.S (Gen. Surgery)
                 </p>
                 <a
-                  className="flex items-center justify-center md:justify-start gap-1 md:pt-2 pt-4 text-[#24426d]"
+                  className="flex items-center justify-center lg:justify-start gap-1 lg:pt-2 pt-4 text-[#24426d]"
                   href="/components/frontend/dr-boi-monika/dr-boi-medhavi"
                 >
-                  <span className="hover:underline text-[16px] font-medium md:ml-5 ml-0">
+                  <span className="hover:underline text-[16px] font-medium lg:ml-5 ml-0">
                     View full bio
                   </span>
                   <span>›</span>
@@ -75,9 +75,9 @@ export default function LaparoscopySurgeons() {
             </div>
 
             {/* Doctor 2 */}
-            <div className="relative md:w-[490px] md:h-[190px] sm:w-[608px] sm:h-[235px] w-75 h-70 bg-white rounded-3xl shadow-md overflow-visible flex flex-col items-center md:block mt-12 md:mt-0">
+            <div className="relative lg:w-[390px] lg:h-[190px] xl:w-[490px] xl:h-[190px]  sm:w-[608px] sm:h-[235px] w-75 h-70 bg-white rounded-3xl shadow-lg overflow-visible flex flex-col items-center lg:block mt-12 lg:mt-0">
               {/* Image Overlapping Top (mobile) */}
-              <div className="absolute md:hidden -top-10 flex justify-center w-full z-10">
+              <div className="absolute lg:hidden -top-10 flex justify-center w-full z-10">
                 <Image
                   alt="Dr. Monika"
                   className="rounded-full border-2 border-[#24426d]"
@@ -90,29 +90,29 @@ export default function LaparoscopySurgeons() {
               {/* Desktop Overlapping (left) */}
               <Image
                 alt="Dr. Monika"
-                className="hidden md:block rounded-full h-30 w-30 absolute -left-12 top-1/2 transform -translate-y-1/2 border-2 border-[#24426d] z-10"
+                className="hidden lg:block rounded-full h-30 w-30 absolute -left-12 top-1/2 transform -translate-y-1/2 border-2 border-[#24426d] z-10"
                 height={96}
                 src="/Monika-tomar.png.avif"
                 width={96}
               />
 
               <div
-                className={`${poppins.className} px-6 py-6 md:pl-23 pl-5 h-full flex flex-col justify-center text-center md:text-left`}
+                className={`${poppins.className} px-6 py-6 lg:pl-23 pl-5 h-full flex flex-col justify-center text-center lg:text-left`}
               >
-                <h1 className="font-semibold text-[20px] text-[#00000099] md:pb-2 pt-8 md:pt-0 md:mt-0 pb-3 ">
+                <h1 className="font-semibold text-[20px] text-[#00000099] lg:pb-2 pt-8 lg:pt-0 lg:mt-0 pb-3 ">
                   Dr. Monika Tomar
                 </h1>
-                <p className="text-[16px] text-[#00000099] md:pb-2 font-normal">
+                <p className="text-[16px] text-[#00000099] lg:pb-2 font-normal">
                   M.B.B.S., M.S (Gen. Surgery)
                 </p>
-                <p className="md:block text-[16px] text-[#00000099] pb-2 font-normal md:leading-tight leading-normal">
+                <p className="lg:block text-[16px] text-[#00000099] pb-2 font-normal lg:leading-tight leading-normal">
                   Chief Consultant – Obstetrics & Gynaecology
                 </p>
                 <a
-                  className="flex items-center justify-center md:justify-start gap-1 mt-2 text-[#24426d]"
+                  className="flex items-center justify-center lg:justify-start gap-1 mt-2 text-[#24426d]"
                   href="/components/frontend/dr-boi-monika/dr-boi-medhavi"
                 >
-                  <span className="hover:underline text-[16px] font-medium md:ml-5 ml-0">
+                  <span className="hover:underline text-[16px] font-medium lg:ml-5 ml-0">
                     View full bio
                   </span>
                   <span>›</span>
