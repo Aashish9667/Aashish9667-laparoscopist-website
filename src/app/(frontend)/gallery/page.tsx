6448f05a606cd0d419b8480aa18656fd904238a6
code with harry <aashish.kumar@96672gmail.com>
2025-10-26 17:58:14 +0530

    feat: gallary section done

--- src/app/(frontend)/gallery/page.tsx
+++ src/app/(frontend)/gallery/page.tsx
@@ -11,6 +11,7 @@ const pressImages = [
   '/live-operative-workshop-5.png',
   '/live-operative-workshop-6.png',
 ];
+
 const COLPOSCOPY = [
   '/colpocopy-cme-cum-workshop-1.png',
   '/colpocopy-cme-cum-workshop-2.png',
@@ -90,127 +91,61 @@ const Videos = [
     videoUrl: 'https://www.youtube.com/watch?v=hLLQ28Vb9YU', // internal URL
   },
 ];
-
 export default function News() {
+  const [activeIndex, setActiveIndex] = React.useState<null | number>(null);
+  const renderImageGrid = (images: string[]) => (
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {images.map((src, i) => (
+        <div
+          className="w-full aspect-[4/3] relative rounded-lg overflow-hidden group shadow-[0px_0px_9px_0px_#2d5289]"
+          key={i}
+          onTouchStart={() => setActiveIndex(i)}
+        >
+          <Image
+            className={`object-cover transform transition-transform duration-300 ${
+              activeIndex === i ? 'scale-110' : 'scale-100'
+            } group-hover:scale-110`}
+            alt={`Image ${i + 1}`}
+            src={src}
+            fill
+          />
+        </div>
+      ))}
+    </div>
+  );
+
   return (
-    <div className="max-w-[1200px] mx-auto md:py-10 py-5 px-4 md:px-8 flex flex-col  gap-10 mt-20">
-      {/* Left side */}
-      <div className="flex1 py-10">
-        {/* Title 1 */}
-        <div className="mb-6">
-          <h1 className="text-2xl font-medium text-[#0f2239] mb-2">LIVE OPERATIVE WORKSHOP</h1>
-        </div>
-
-        {/* Image Grid */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 grid-col-3 gap-4">
-          {pressImages.map((src, i) => (
-            <div
-              className="w-full aspect-[4/3] relative rounded-lg overflow-hidden group shadow-[0px_0px_9px_0px_#2d5289]"
-              key={i}
-            >
-              <Image
-                alt={`Press Release ${i + 1}`}
-                className="object-cover transform transition-transform duration-500 group-hover:scale-110 active:scale-110 focus:scale-110"
-                src={src}
-                fill
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="flex-1 py-10">
-        {/* Title 2 */}
-        <div className="mb-6">
-          <h1 className="text-2xl font-medium text-[#0f2239] mb-2">COLPOSCOPY CME CUM WORKSHOP</h1>
-        </div>
-
-        {/* Image Grid */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-4">
-          {COLPOSCOPY.map((src, i) => (
-            <div
-              className="w-full aspect-[4/3] relative rounded-lg overflow-hidden group shadow-[0px_0px_9px_0px_#2d5289]"
-              key={i}
-            >
-              <Image
-                alt={`Press Release ${i + 1}`}
-                className="object-cover transform transition-transform duration-500 group-hover:scale-110 active:scale-110 focus:scale-110"
-                src={src}
-                fill
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="flex-1 py-10">
-        {/* Title 3 */}
-        <div className="mb-6">
-          <h1 className="text-2xl font-medium text-[#0f2239] mb-2">MIPH</h1>
-        </div>
-
-        {/* Image Grid */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-4">
-          {MIPH.map((src, i) => (
-            <div
-              className="w-full aspect-[4/3] relative rounded-lg overflow-hidden group shadow-[0px_0px_9px_0px_#2d5289]"
-              key={i}
-            >
-              <Image
-                alt={`Press Release ${i + 1}`}
-                className="object-cover transform transition-transform duration-500 group-hover:scale-110 active:scale-110 focus:scale-110"
-                src={src}
-                fill
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="flex-1 py-10">
-        {/* Title 4 */}
-        <div className="mb-6">
-          <h1 className="text-2xl font-medium text-[#0f2239] mb-2">SAGES</h1>
-        </div>
-
-        {/* Image Grid */}
-        <div className="grid sm:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-4">
-          {SAGES.map((src, i) => (
-            <div
-              className="w-full aspect-[4/3] relative rounded-lg overflow-hidden group shadow-[0px_0px_9px_0px_#2d5289]"
-              key={i}
-            >
-              <Image
-                alt={`Press Release ${i + 1}`}
-                className="object-cover transform transition-transform duration-500 group-hover:scale-110 active:scale-110 focus:scale-110"
-                src={src}
-                fill
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="flex-1 py-10">
-        {/* Title 5 */}
-        <div className="mb-6">
-          <h1 className="text-2xl font-medium text-[#0f2239] mb-2">AIIMS</h1>
-        </div>
-
-        {/* Image Grid */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-4">
-          {AIIMS.map((src, i) => (
-            <div
-              className="w-full aspect-[4/3] relative rounded-lg overflow-hidden group shadow-[0px_0px_9px_0px_#2d5289]"
-              key={i}
-            >
-              <Image
-                alt={`Press Release ${i + 1}`}
-                className="object-cover transform transition-transform duration-500 group-hover:scale-110 active:scale-110 focus:scale-110"
-                src={src}
-                fill
-              />
-            </div>
-          ))}
-        </div>
+    <div className="max-w-[1200px] mx-auto py-10 px-4 md:px-8 flex flex-col gap-10 mt-20 ">
+      {/* section 1 */}
+      <div className="py-10">
+        <h1 className="text-2xl font-medium text-[#0f2239] mb-4">LIVE OPERATIVE WORKSHOP</h1>
+        {renderImageGrid(pressImages)}
       </div>
 
+      {/* section 2 */}
+      <div className="py-10">
+        <h1 className="text-2xl font-medium text-[#0f2239] mb-4">COLPOSCOPY CME CUM WORKSHOP</h1>
+        {renderImageGrid(COLPOSCOPY)}
+      </div>
+
+      {/* section 3 */}
+      <div className="py-10">
+        <h1 className="text-2xl font-medium text-[#0f2239] mb-4">MIPH</h1>
+        {renderImageGrid(MIPH)}
+      </div>
+
+      {/* section 4 */}
+      <div className="py-10">
+        <h1 className="text-2xl font-medium text-[#0f2239] mb-4">SAGES</h1>
+        {renderImageGrid(SAGES)}
+      </div>
+
+      {/* section 5 */}
+      <div className="py-10">
+        <h1 className="text-2xl font-medium text-[#0f2239] mb-4">AIIMS</h1>
+        {renderImageGrid(AIIMS)}
+      </div>
+      {/* section 6 */}
       <div className="flex-1 py-10">
         {/* Title 6 */}
         <div className="mb-6">
