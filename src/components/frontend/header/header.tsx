26a17863872d79a2f286ed32f1cd3425f4a7255d
code with harry <aashish.kumar@96672gmail.com>
2025-11-10 11:00:54 +0530

    feat: change in header file

--- src/components/frontend/header/header.tsx
+++ src/components/frontend/header/header.tsx
@@ -13,11 +13,37 @@ import {
   NavigationMenuLink,
 } from '@/components/ui/navigation-menu';
 import { Button } from '@/components/ui/button';
+import { ChevronUp } from 'lucide-react';
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
 });
+
+const desktopServices = [
+  { href: '/services/laparoscopy-surgery', label: 'Laparoscopy Surgery' },
+  { href: '/services/laparoscopic-gynae', label: 'Laparoscopic Gynae' },
+  { href: '/services/proctology', label: 'Proctology' },
+  { href: '/services/endourology', label: 'Endourology' },
+];
+const desktopMainLinks = [
+  { href: '/about-us', label: 'About Us' },
+  { href: '/news-media', label: 'News & Media' },
+  { href: '/gallery', label: 'Gallery' },
+  { href: '/blogs', label: 'Blogs' },
+];
+const serviceLinksmobile = [
+  { href: '/services/laparoscopy-surgery', label: 'Laparoscopy Surgery' },
+  { href: '/services/laparoscopic-gynae', label: 'Laparoscopic Gynae' },
+  { href: '/services/proctology', label: 'Proctology' },
+  { href: '/services/endourology', label: 'Endourology' },
+];
+const othersLinksmobile = [
+  { href: '/about-us', label: 'About Us' },
+  { href: '/news-media', label: 'News & Media' },
+  { href: '/gallery', label: 'Gallery' },
+  { href: '/blogs', label: 'Blogs' },
+];
 
 export default function Navbar() {
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
@@ -47,216 +73,92 @@ export default function Navbar() {
             <NavigationMenuList className="flex gap-4 mx-10 md:mx-8">
               {/* Services Dropdown */}
               <NavigationMenuItem>
-                <NavigationMenuTrigger className="!text-[#22426e] text-[14px] font-medium !bg-transparent !hover:bg-transparent hover:underline hover:decoration-[#22426e] hover:decoration-1 hover:underline-offset-4 hover:text-[#22426e]">
+                <NavigationMenuTrigger className="!text-[#22426e] text-[14px] font-medium !bg-transparent !hover:bg-transparent hover:underline hover:decoration-[#22426e] hover:decoration-1 hover:underline-offset-1 hover:text-[#22426e]">
                   Services
                 </NavigationMenuTrigger>
                 <div className="rounded-none">
                   <NavigationMenuContent className="!bg-white data-[state=open]:!rounded-none data-[state=open]:!shadow-2xl !border-0 !p-0 min-w-[220px]">
-                    <NavigationMenuLink asChild>
-                      <Link
-                        className="block px-5 py-4 hover:bg-gray-50 !text-[#22426e] text-[14px] font-medium "
-                        href="/services/laparoscopy-surgery"
-                      >
-                        Laparoscopy Surgery
-                      </Link>
-                    </NavigationMenuLink>
-                    <NavigationMenuLink asChild>
-                      <Link
-                        className="block px-5 py-4 hover:bg-gray-50 !text-[#22426e] text-[14px] font-medium "
-                        href="/services/laparoscopic-gynae"
-                      >
-                        Laparoscopic Gynae
-                      </Link>
-                    </NavigationMenuLink>
-                    <NavigationMenuLink asChild>
-                      <Link
-                        className="block px-5 py-4 hover:bg-gray-50 !text-[#22426e] text-[14px] font-medium "
-                        href="/services/proctology"
-                      >
-                        Proctology
-                      </Link>
-                    </NavigationMenuLink>
-                    <NavigationMenuLink asChild>
-                      <Link
-                        className="block px-5 py-4 hover:bg-gray-50 !text-[#22426e] text-[14px] font-medium"
-                        href="/services/endourology"
-                      >
-                        Endourology
-                      </Link>
-                    </NavigationMenuLink>
+                    {desktopServices.map((item) => (
+                      <NavigationMenuLink key={item.href} asChild>
+                        <Link
+                          className="block px-5 py-4 hover:bg-gray-50 !text-[#22426e] text-[14px] font-medium "
+                          href={item.href}
+                        >
+                          {item.label}
+                        </Link>
+                      </NavigationMenuLink>
+                    ))}
                   </NavigationMenuContent>
                 </div>
               </NavigationMenuItem>
 
               {/* Other Links */}
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link
-                    className=" text-[14px] font-medium  hover:text-[#22426e] !text-[#22426e]  hover:bg-transparent hover:underline hover:decoration-[#22426e] hover:decoration-1 hover:underline-offset-4"
-                    href="/about-us"
-                  >
-                    About Us
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link
-                    className="text-[14px] font-medium bg-transparent hover:text-[#22426e] !text-[#22426e]  hover:bg-transparent hover:underline hover:decoration-[#22426e] hover:decoration-1 hover:underline-offset-4"
-                    href="/news-media"
-                  >
-                    News & Media
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link
-                    className=" text-[14px] font-medium bg-transparent hover:text-[#22426e] !text-[#22426e]  hover:bg-transparent hover:underline hover:decoration-[#22426e] hover:decoration-1 hover:underline-offset-4"
-                    href="/gallery"
-                  >
-                    Gallery
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link
-                    className="text-[14px] font-medium bg-transparent hover:text-[#22426e] !text-[#22426e]  hover:bg-transparent hover:underline hover:decoration-[#22426e] hover:decoration-1 hover:underline-offset-4"
-                    href="/blogs"
-                  >
-                    Blogs
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
+              {desktopMainLinks.map((item) => (
+                <NavigationMenuItem key={item.href}>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      className="text-[14px] font-medium bg-transparent hover:text-[#22426e] !text-[#22426e]  hover:bg-transparent hover:underline hover:decoration-[#22426e] hover:decoration-1 hover:underline-offset-1"
+                      href={item.href}
+                    >
+                      {item.label}
+                    </Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              ))}
             </NavigationMenuList>
           </NavigationMenu>
         </div>
-
         {/* Mobile menu button */}
         <div className="flex [@media(min-width:986px)]:hidden">
           <Button
-            className="bg-white text-[#000000ed] hover:bg-transparent focus-visible:ring-0 active:bg-transparent !text-xl !font-extrabold "
+            className="bg-white text-[#000000ed] hover:bg-transparent focus-visible:ring-0 active:bg-transparent !text-xl !font-light "
             onClick={() => setIsDrawerOpen(!isDrawerOpen)}
             variant="ghost"
           >
-            {isDrawerOpen ? <FiX size={34} /> : <FiMenu size={34} />}
+            {isDrawerOpen ? (
+              <FiX className="!font-light" size={34} strokeWidth={3} />
+            ) : (
+              <FiMenu className="!font-light" size={34} strokeWidth={3} />
+            )}
           </Button>
         </div>
       </div>
 
       {/* Mobile Drawer */}
       {isDrawerOpen && (
-        <div className="absolute top-13 shadow-2xl right-6 w-[250px] bg-white rounded-sm py-4 px-5 flex flex-col gap-3 z-50 [@media(min-width:986px)]:hidden text-left">
-          <NavigationMenu>
-            <NavigationMenuList className="flex flex-col gap-2 items-start">
-              {/* Services Dropdown */}
-              <NavigationMenuItem className="w-full">
-                <NavigationMenuTrigger
-                  className="w-full flex items-center justify-between !text-[#22426e] text-[14px] px-2 gap-30 !bg-transparent !hover:bg-transparent !focus:bg-transparent !ring-0 !shadow-none"
-                  onClick={() => setShowServices(!showServices)}
-                >
-                  <span className="text-left !bg-transparent !hover:bg-transparent !hover:underline !hover:decoration-[#22426e] !font-normal">
-                    Services
-                  </span>
-                </NavigationMenuTrigger>
+        <div className="absolute top-13 shadow-2xl right-6 w-[250px] bg-white rounded-sm py-4 px-4 flex flex-col gap-4 z-50 [@media(min-width:986px)]:hidden text-left">
+          <button
+            className="flex justify-between text-[#22426e] text-[14px] w-full bg-transparent hover:underline font-normal"
+            onClick={() => setShowServices(!showServices)}
+            type="button"
+          >
+            Services
+            <ChevronUp
+              className={`transition-transform duration-900 ${showServices ? 'rotate-180' : 'rotate-0'} text-[#22426e] size-3`}
+              strokeWidth={4}
+            />
+          </button>
 
-                {/* Render services text/content below the trigger */}
-                {showServices && (
-                  <div className="py-2 mt-2 flex flex-col gap-1 px-4 text-[14px] relative">
-                    {/* vertical line */}
-                    <div className="absolute left-2 top-1 bottom-0  w-[1px]  bg-gray-100" />
-                    <Link
-                      className="py-1 !text-[#22426e] ml-1 text-left hover:underline"
-                      href="/services/laparoscopy-surgery"
-                      onClick={() => setIsDrawerOpen(false)}
-                    >
-                      Laparoscopy Surgery
-                    </Link>
+          {showServices && (
+            <div className="flex flex-col gap-2 text-[14px] text-[#22426e] border-l border-gray-200 pl-3">
+              {serviceLinksmobile.map((item) => (
+                <Link href={item.href} key={item.href} onClick={() => setIsDrawerOpen(false)}>
+                  {item.label}
+                </Link>
+              ))}
+            </div>
+          )}
 
-                    <Link
-                      className="py-1 !text-[#22426e] ml-1 text-left hover:underline"
-                      href="/services/laparoscopic-gynae"
-                      onClick={() => setIsDrawerOpen(false)}
-                    >
-                      Laparoscopic Gynae
-                    </Link>
-
-                    <Link
-                      className="py-1 !text-[#22426e] ml-1 text-left hover:underline"
-                      href="/services/proctology"
-                      onClick={() => setIsDrawerOpen(false)}
-                    >
-                      Proctology
-                    </Link>
-
-                    <Link
-                      className="py-1 !text-[#22426e] ml-1 text-left hover:underline"
-                      href="/services/endourology"
-                      onClick={() => setIsDrawerOpen(false)}
-                    >
-                      Endourology
-                    </Link>
-                  </div>
-                )}
-              </NavigationMenuItem>
-
-              {/* Other Links */}
-              <NavigationMenuItem className="w-full">
-                <NavigationMenuLink asChild>
-                  <Link
-                    className="text-[#22426e] text-[14px] block w-full text-left !hover:underline !hover:decoration-[#22426e]"
-                    href="/about-us"
-                    onClick={() => setIsDrawerOpen(false)}
-                  >
-                    About Us
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-
-              <NavigationMenuItem className="w-full">
-                <NavigationMenuLink asChild>
-                  <Link
-                    className="text-[#22426e] text-[14px] block w-full text-left !hover:underline !hover:decoration-[#22426e]"
-                    href="/news-media"
-                    onClick={() => setIsDrawerOpen(false)}
-                  >
-                    News & Media
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-
-              <NavigationMenuItem className="w-full">
-                <NavigationMenuLink
-                  className="!hover:underline underline-offset-2 !hover:decoration-[#22426e]"
-                  asChild
-                >
-                  <Link
-                    className="text-[#22426e] text-[14px] block w-full text-left !hover:underline !hover:decoration-[#22426e]"
-                    href="/gallery"
-                    onClick={() => setIsDrawerOpen(false)}
-                  >
-                    Gallery
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-
-              <NavigationMenuItem className="w-full bg-transparent !hover:underline">
-                <NavigationMenuLink asChild>
-                  <Link
-                    className="text-[#22426e] text-[14px] block w-full text-left !hover:underline !hover:decoration-[#22426e]"
-                    href="/blogs"
-                    onClick={() => setIsDrawerOpen(false)}
-                  >
-                    Blogs
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
+          {othersLinksmobile.map((item) => (
+            <Link
+              className="text-[#22426e] text-[14px] hover:underline"
+              href={item.href}
+              key={item.href}
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              {item.label}
+            </Link>
+          ))}
         </div>
       )}
     </nav>
